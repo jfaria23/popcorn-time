dd68ae5b52c8353711e5725d33848fb07bb36f52
Joao Faria <joaorafaria@gmail.com>
2023-06-20 15:54:01 +0100

    third commit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -17,23 +17,42 @@ function Main() {
     setMoviesToDisplay(newList);
   };
 
+  let message = "";
+  if (moviesToDisplay.length > 0) {
+    message = <h1>Number of movies: {moviesToDisplay.length}</h1>;
+  } else {
+    message = <h1>Sorry no movies to display</h1>;
+  }
   return (
     <div className="Main">
-      <h1>Number of movies: {moviesToDisplay.length}</h1>
+      {message}
+
       {moviesToDisplay.map((movieObj) => {
         return (
           <div key={movieObj.id} className="card">
             <p>{movieObj.title}</p>
             <p>{movieObj.year}</p>
-            <p>{movieObj.rating}</p>
+            <p>Rating: {movieObj.rating}</p>
 
-            <button
-              onClick={() => {
-                deleteMovie(movieObj.id);
-              }}
-            >
-              Delete this movie
-            </button>
+            {movieObj.imgURL ? (
+              <img src={movieObj.imgURL} />
+            ) : (
+              <img src="https://dummyimage.com/182x268/ffffff/000000" />
+            )}
+
+            <img src={movieObj.imgURL} />
+
+            {movieObj.rating >= 8 && <h2>This movie is amazing</h2>}
+
+            <p>
+              <button
+                onClick={() => {
+                  deleteMovie(movieObj.id);
+                }}
+              >
+                Delete this movie
+              </button>
+            </p>
           </div>
         );
       })}
