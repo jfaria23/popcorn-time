d56e75d8c0cb042f7a8040f59370346be4ee05cc
Joao Faria <joaorafaria@gmail.com>
2023-06-20 11:53:24 +0100

    3 iterations done

---
+++ src/Components/Footer.js
@@ -0,0 +1,4 @@
+function Footer() {
+  return <h1>This is the Footer</h1>;
+}
+export default Footer;
---
+++ src/Components/Main.js
@@ -0,0 +1,4 @@
+function Main() {
+  return <h1>This is the Main part</h1>;
+}
+export default Main;
