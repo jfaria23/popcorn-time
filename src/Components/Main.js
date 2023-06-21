f578d84a3a8f2cb43ac65591a29d4128a9dd51d0
Joao Faria <joaorafaria@gmail.com>
2023-06-21 10:53:24 +0100

    delete movies and Movie component created

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,62 +1,47 @@
 import { useState } from "react";
 import movies from "../data/movies.json";
+
 import "./Main.css";
+import Movie from "./Movie";
 
 function Main() {
   const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
   const deleteMovie = (movieId) => {
-    console.log("DOING SHIT", movieId);
+    // console.log("deleting movie with id....", movieId)
+
+    // moviesToDisplay.push(); // NEVER MODIFY STATE DIRECTLY !
+
     const newList = moviesToDisplay.filter((element) => {
-      if (element.id !== movieId) {
-        return true;
-      } else {
-        return false;
-      }
+      return element.id !== movieId;
     });
+
     setMoviesToDisplay(newList);
   };
 
+  // Conditional Rendering: option A (Element Variables)
   let message = "";
   if (moviesToDisplay.length > 0) {
     message = <h1>Number of movies: {moviesToDisplay.length}</h1>;
   } else {
-    message = <h1>Sorry no movies to display</h1>;
+    message = <h1>Sorry, no movies to display</h1>;
   }
+
   return (
     <div className="Main">
       {message}
 
       {moviesToDisplay.map((movieObj) => {
         return (
-          <div key={movieObj.id} className="card">
-            <p>{movieObj.title}</p>
-            <p>{movieObj.year}</p>
-            <p>Rating: {movieObj.rating}</p>
-
-            {movieObj.imgURL ? (
-              <img src={movieObj.imgURL} />
-            ) : (
-              <img src="https://dummyimage.com/182x268/ffffff/000000" />
-            )}
-
-            <img src={movieObj.imgURL} />
-
-            {movieObj.rating >= 8 && <h2>This movie is amazing</h2>}
-
-            <p>
-              <button
-                onClick={() => {
-                  deleteMovie(movieObj.id);
-                }}
-              >
-                Delete this movie
-              </button>
-            </p>
-          </div>
+          <Movie
+            key={movieObj.id}
+            movieDetails={movieObj}
+            callbackToDelete={deleteMovie}
+          />
         );
       })}
     </div>
   );
 }
+
 export default Main;
---
+++ src/Components/Movie.js
@@ -0,0 +1,30 @@
+function Movie(props) {
+  return (
+    <div className="card">
+      <p>{props.movieDetails.title}</p>
+      <p>Rating: {props.movieDetails.rating}</p>
+
+      {props.movieDetails.imgURL ? (
+        <img src={props.movieDetails.imgURL} />
+      ) : (
+        <img src="https://dummyimage.com/182x268/ffffff/000000" />
+      )}
+
+      {props.movieDetails.rating > 8 && <p>RECOMMENDED</p>}
+
+      <p>
+        {/* <button onClick={() => { deleteMovie(props.movieDetails.id) }}>Delete this movie</button> */}
+
+        <button
+          onClick={() => {
+            props.callbackToDelete(props.movieDetails.id);
+          }}
+        >
+          Delete this movie
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Movie;
